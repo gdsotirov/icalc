0f95a8299b41dc3bf7e75779617a328b44b7c258
Georgi D. Sotirov <gdsotirov@dir.bg>
2005-05-23 20:53:45 +0000

    Fix serious calculation errors

--- icc.js
+++ icc.js
@@ -2,7 +2,7 @@
  * ---
  * Written by George D. Sotirov (gdsotirov@dir.bg)
  * Version: 0.1.0
- * $Id: icc.js,v 1.2 2005/04/20 18:53:47 gsotirov Exp $
+ * $Id: icc.js,v 1.3 2005/05/23 20:53:45 gsotirov Exp $
  */
 
 /* Function   : calc_interest
@@ -14,20 +14,20 @@
  */
 function calc_interest(amount, type, interest, period) {
   if ( period % type )
-     return 0;
-  var iperiods = period / type;
+    return 0;
+
   var acc = amount;
   var Rows = new Array();
-  for ( var i = 1; i <= iperiods; ++i ) {
-    var month = i * type;
-    var profit;
-    if ( type < 12 )
-      profit = acc * interest/12/100;
+
+  for ( var i = 1; i <= period/type; ++i ) {
+    if ( type <= 12 )
+      var profit = acc * interest/(12/type)/100;
     else
-      profit = acc * interest/100;
+      var profit = acc * interest/100;
     acc += profit;
-    var row = new Array(month, acc, profit);
+    var row = new Array(i * type, acc, profit, acc - amount);
     Rows[i] = row;
   }
   return Rows;
 }
+
