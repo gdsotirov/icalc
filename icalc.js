a7577702d7a41720a1b8ef8c87e2477427d26aad
Georgi D. Sotirov <gdsotirov@dir.bg>
2006-02-15 20:19:38 +0000

    GPL-ization

--- icalc.js
+++ icalc.js
@@ -1,7 +1,21 @@
 /* Interest calculator Web Interface
- * ---
- * Written by George D. Sotirov (gdsotirov@dir.bg)
- * $Id: icalc.js,v 1.13 2006/02/02 19:55:08 gsotirov Exp $
+ * Copyright (C) 2004-2006  Georgi D. Sotirov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: icalc.js,v 1.14 2006/02/15 20:19:38 gsotirov Exp $
  */
 
 /* This are the interests for the main currencyes as defined by
--- icc.js
+++ icc.js
@@ -1,8 +1,21 @@
 /* Interest calculator
- * ---
- * Written by George D. Sotirov (gdsotirov@dir.bg)
- * Version: 0.1.0
- * $Id: icc.js,v 1.7 2006/02/02 19:56:39 gsotirov Exp $
+ * Copyright (C) 2004-2006  Georgi D. Sotirov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: icc.js,v 1.8 2006/02/15 20:19:38 gsotirov Exp $
  */
 
 /* Function   : calc_interest
